48e11c0b7a4abaedc67e6aefdee4e564d18c6b64
Jakub <jakub.kanclerz@gmail.com>
2019-11-23 14:22:50 +0100

    introduce some empji

--- src/main/java/pl/jkan/pp5/creditcard/Application.java
+++ src/main/java/pl/jkan/pp5/creditcard/Application.java
@@ -2,6 +2,6 @@ package pl.jkan.pp5.creditcard;
 
 public class Application {
     public static void main(String[] args) {
-        System.out.println("It woks :):>");
+        System.out.println("It woks :):> :(");
     }
 }
